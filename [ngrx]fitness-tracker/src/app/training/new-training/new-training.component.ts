8d9e37e438180bb29e1173db2771b2e5890bc3cf
Dimitar Mitev <dnmitev@gmail.com>
2018-05-03 17:10:30 +0300

    added speech recognition

--- [ngrx]fitness-tracker/src/app/training/new-training/new-training.component.ts
+++ [ngrx]fitness-tracker/src/app/training/new-training/new-training.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -9,6 +9,8 @@ import { Exercise } from '../exercise.model';
 import { UIService } from '../../shared/ui.service';
 import * as fromTraining from '../training.reducer';
 import * as fromRoot from '../../app.reducer';
+import { debug } from 'util';
+import { DEFAULT_BREAKPOINTS } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-new-training',
@@ -18,14 +20,16 @@ import * as fromRoot from '../../app.reducer';
 export class NewTrainingComponent implements OnInit {
   exercises$: Observable<Exercise[]>;
   isLoading$: Observable<boolean>;
+  @Output() isListening: EventEmitter<boolean> = new EventEmitter(true);
 
   constructor(
     private trainingService: TrainingService,
     private uiService: UIService,
     private store: Store<fromTraining.State>
-  ) {}
+  ) { }
 
   ngOnInit() {
+    this.isListening.emit(false);
     this.isLoading$ = this.store.select(fromRoot.getIsLoading);
     this.exercises$ = this.store.select(fromTraining.getAvailableExercises);
     this.fetchExercises();
@@ -37,5 +41,60 @@ export class NewTrainingComponent implements OnInit {
 
   onStartTraining(form: NgForm) {
     this.trainingService.startExercise(form.value.exercise);
+  }
+
+  onMicBtnClick(form: NgForm) {
+    var component = this;
+    component.isListening.emit(true);
+
+    this.exercises$.subscribe(exercises => {
+
+      // TODO: Refactor to have better structure; get into separate reducer or service
+      var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+      var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+      var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+
+      var trainings = exercises.map(e => e.name.toLowerCase());
+      var grammar = '#JSGF V1.0; grammar trainings; public <trainig> = ' + trainings.join(' | ') + ' ;'
+
+      var recognition = new SpeechRecognition();
+      var speechRecognitionList = new SpeechGrammarList();
+
+      speechRecognitionList.addFromString(grammar, 1);
+
+      recognition.grammars = speechRecognitionList;
+      recognition.lang = 'en-US';
+      recognition.interimResults = false;
+      recognition.maxAlternatives = 1;
+
+      recognition.start();
+
+      recognition.onresult = function (event) {
+        component.isListening.emit(false);
+        var last = event.results.length - 1;
+        var training = event.results[last][0].transcript;
+        var found = exercises.filter(e => e.name.toLowerCase() == training.toLowerCase());
+        if (!found || !found.length) {
+          component.uiService.showSnackbar(`Excercise: ${training} was not found! Please try again.`, null, 3000);
+        } else {
+          component.selected = found[0].id;
+        }
+      }
+
+      recognition.onspeechend = function () {
+        component.isListening.emit(false);
+        recognition.stop();
+      }
+
+      recognition.onnomatch = function (event) {
+        component.isListening.emit(false);
+        component.uiService.showSnackbar(`Training did not match!`, null, 3000);
+      }
+
+      recognition.onerror = function (event) {
+        component.isListening.emit(false);
+        component.uiService.showSnackbar('Training not found. Please try again!', null, 3000);
+      }
+    });
   }
 }
